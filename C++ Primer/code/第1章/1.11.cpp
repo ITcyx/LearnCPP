e834b421f34a0ff196325d62fbb8102d86fd4067
Andy Cai <1781953879@qq.com>
2021-03-01 22:45:38 +0800

    add C++ Primer code 1.11.cpp

---
+++ C++ Primer/code/第1章/1.11.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num1, num2, val;
+	std::cout << "Enter two numbers:" << std::endl;
+	std::cin >> num1 >> num2;
+	val = num1;
+	while (val <= num2)
+	{
+		std::cout << val << std::endl;
+		++val;
+	}
+	return 0;
+}
