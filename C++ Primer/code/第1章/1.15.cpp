9421919db632658ebd12c64ed570502188556857
Andy Cai <1781953879@qq.com>
2021-03-01 23:27:10 +0800

    add C++ Primer code 1.5.cpp

---
+++ C++ Primer/code/第1章/1.15.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+void foo(int val)
+{
+	std::wcout << val << std::endl;
+}
+
+// synatx error: symbol ) is missing behind of main
+int main({
+	// synatx error: use symbol : instead of symbol ; behind of endl
+	std::cout << "Read each file." << std::endl:
+	// synatx error: symbol "" are missing from both sides of string literal
+	std::cout << Update master. << std::endl;
+	// synatx error: symbol << are missing behind of string literal
+	std::cout << "Write new master"std::endl;
+	// type error: pass a string literal to a function with a parameter of int
+	foo("hello");
+	int v1 = 0, v2 = 0;
+	// declaration error: use v instead of v1
+	std::cin >> v >> v2;
+	//declaration error: cout is not define, maybe is std::cout
+	cout << v1 + v2 << std::endl;
+	// synatx error: symbol ; is missing behind of 0
+	return 0
+}
