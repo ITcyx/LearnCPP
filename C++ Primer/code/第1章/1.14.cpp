4ff09c73f49d2fcfb31e639e0ff3a3a2dab175fa
Andy Cai <1781953879@qq.com>
2021-03-01 23:26:13 +0800

    add C++ Primer code 1.14.cpp

---
+++ C++ Primer/code/第1章/1.14.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int val = 0;
+	while (val <= 10)
+	{
+		std::cout << val << std::endl;
+		++val;
+	}
+	for (val = 0; val <= 10; ++val)
+		std::cout << val << std::endl;
+	return 0;
+}
