efda3571dbad4078e9a56d63663d5b857ec2c3ff
Andy Cai <1781953879@qq.com>
2021-03-07 19:32:43 +0800

    add C++ Primer code 1.23.cpp

---
+++ C++ Primer/code/第1章/1.23.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	Sales_item total, trans;
+	int i = 0;
+	if (std::cin >> total)
+	{
+		++i;
+		while (std::cin >> trans)
+		{
+			if (total.isbn() == trans.isbn())
+				++i;
+			else
+			{
+				std::cout << total.isbn() << " " << i << std::endl;
+				i = 1;
+				total = trans;
+			}
+		}
+		std::cout << total.isbn() << " " << i << std::endl;
+	}
+	return 0;
+}
