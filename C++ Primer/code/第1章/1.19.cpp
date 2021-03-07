1604ebc206cc4535482aad6d28d631bc45aa352e
Andy Cai <1781953879@qq.com>
2021-03-07 16:17:46 +0800

    add C++ Primer code 1.19.cpp

---
+++ C++ Primer/code/第1章/1.19.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num1, num2, val, max;
+	std::cout << "Enter two numbers:" << std::endl;
+	std::cin >> num1 >> num2;
+	if (num1 < num2)
+	{
+		val = num1;
+		max = num2;
+	}
+	else
+	{
+		val = num2;
+		max = num1;
+	}
+	while (val <= max)
+	{
+		std::cout << val << std::endl;
+		++val;
+	}
+	return 0;
+}
