932b7f9b3df81c24e00ad11383b1ae41b2806153
Andy Cai <1781953879@qq.com>
2021-03-07 17:51:56 +0800

    add C++ Primer code 1.22.cpp

---
+++ C++ Primer/code/第1章/1.22.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "Sales_item.h"
+
+int main()
+{
+	Sales_item book, item;
+	std::cin >> book;
+	while (std::cin >> item)
+		book += item;
+	std::cout << book << std::endl;
+	return 0;
+}
