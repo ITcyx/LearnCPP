647dd477e799b302ceff3d0f54491db40599fa85
Andy Cai <1781953879@qq.com>
2021-03-22 15:46:32 +0800

    add C++ Primer code 第2章 2.7.cpp

---
+++ C++ Primer/code/第2章/2.7.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+	char a[] = "Who goes with F\145rgus?\012";
+	double b = 3.14e1L;
+	float c = 1024f;  // Here is an error. c is an illegal float.
+	double d = 3.14L;
+	return 0;
+}
