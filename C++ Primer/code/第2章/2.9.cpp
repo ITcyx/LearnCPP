e67cd2600841b1443a672a910c6d66be5fab9654
Andy Cai <1781953879@qq.com>
2021-03-23 00:21:56 +0800

    add C++ Primer code 第2章 2.9.cpp

---
+++ C++ Primer/code/第2章/2.9.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+	std::cin >> int input_value;  // Here has an error.
+	int i_b = { 3.14 };  // Here has an error.
+	double salary = wage = 9999.99;
+	int i_d = 3.14;
+	return 0;
+}
