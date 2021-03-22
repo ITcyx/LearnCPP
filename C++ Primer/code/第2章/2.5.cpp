8476aabd27fbb5337bb6b9edf25b388fa2278ca8
Andy Cai <1781953879@qq.com>
2021-03-22 14:37:59 +0800

    add C++ Primer code 第2章 2.5.cpp

---
+++ C++ Primer/code/第2章/2.5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	char a1 = 'a';
+	wchar_t a2 = L'a';
+	char a3[] = "a";
+	wchar_t a4[] = L"a";
+	int b1 = 10;
+	unsigned b2 = 10u;
+	long b3 = 10L;
+	unsigned long b4 = 10uL;
+	int b5 = 012;
+	int b6 = 0xC;
+	double c1 = 3.14;
+	float c2 = 3.14f;
+	double c3 = 3.14L;
+	int d1 = 10;
+	unsigned d2 = 10u;
+	double d3 = 10.;
+	double d4 = 10e-2;
+	return 0;
+}
