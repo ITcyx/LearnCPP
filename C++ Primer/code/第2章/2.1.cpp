26d887d71ad1a76dc92f3541629f3942ae45107d
Andy Cai <1781953879@qq.com>
2021-03-22 11:11:43 +0800

    add C++ Primer code 第2章 2.1.cpp

---
+++ C++ Primer/code/第2章/2.1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int num_int;
+	long num_long;
+	long long num_long_long;
+	short num_short;
+	unsigned int num_unsigned_int;
+	unsigned long num_unsigned_long;
+	unsigned long long num_unsigned_long_long;
+	unsigned short num_unsigned_short;
+	float num_float;
+	double num_double;
+	return 0;
+}
