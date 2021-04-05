8af32f7f99e2df5b6949ceee83753042eabdc6bb
Andy Cai <1781953879@qq.com>
2021-04-05 16:18:57 +0800

    add C++ Primer code 第2章 2.22.cpp

---
+++ C++ Primer/code/第2章/2.22.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	int val = 1;
+	int *p = &val;
+	if (p)
+		std::cout << "p is not a nullptr!" << std::endl;
+	if (*p)
+		std::cout << "The int object which is p point to is not 0!" << std::endl;
+	return 0;
+}
