73fadc21a594191878a7bf18ede0511062a33cb1
Andy Cai <1781953879@qq.com>
2021-04-05 16:42:19 +0800

    add C++ Primer code 第2章 2.25.cpp

---
+++ C++ Primer/code/第2章/2.25.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+	int* aip, ai, &ar = ai;  // aip is a int pointer, ai is a int object, ar is a int reference whitch is refering to ai
+	int bi, *bip = 0;  // bi is a int object, bip is a int pointer
+	int* cip, cip2;  // cip is a int pointer cip2 is a int object
+	return 0;
+}
