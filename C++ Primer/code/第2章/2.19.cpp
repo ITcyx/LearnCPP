0a3c89e4a6aaf13c7af805b479f0a3e782904524
Andy Cai <1781953879@qq.com>
2021-04-05 16:10:26 +0800

    add C++ Primer code 第2章 2.19.cpp

---
+++ C++ Primer/code/第2章/2.19.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int ival = 1, jval = 2;
+	int &val = ival;
+	val = 2;
+	std::cout << ival << std::endl;
+	int *pv = &ival;
+	*pv = 3;
+	std::cout << ival << std::endl;
+	pv = &jval;
+	*pv = 4;
+	std::cout << jval << std::endl;
+	return 0;
+}
