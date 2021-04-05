a7f162db4542a502342d328338c3b4e7058ab507
Andy Cai <1781953879@qq.com>
2021-04-05 16:27:12 +0800

    add C++ Primer code 第2章 2.23.cpp

---
+++ C++ Primer/code/第2章/2.23.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	int val = 1;
+	int *p;
+	if (*p)  // Use *p to test whether p is pointing to a legal int object. If p is pointing to a illegal int object, the compiler will report an error
+		std::cout << "p is pointing to a legal int object!" << std::endl;
+	else
+		std::cout << "p is pointing to a legal int object!" << std::endl;
+	return 0;
+}
