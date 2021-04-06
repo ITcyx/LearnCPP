728afe9adf1bc91d9cbf07aa564b21270e73fe30
Andy Cai <1781953879@qq.com>
2021-04-06 23:14:38 +0800

    add C++ Primer code 第2章 2.28.cpp

---
+++ C++ Primer/code/第2章/2.28.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	int i, *const cp;  // cp is an error
+	int *p1, *const p2;  // p2 is an error
+	const int ic, &r = ic;  // ic is an error
+	const int *const p3;  // Error
+	const int *p;
+	return 0;
+}
