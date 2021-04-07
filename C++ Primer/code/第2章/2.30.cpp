2557829361f6bae06d74ad6f4ac68dc0e7255211
Andy Cai <1781953879@qq.com>
2021-04-07 23:11:56 +0800

    add C++ Primer code 第2章 2.30.cpp

--- C++ Primer/code/第2章/2.30.cpp
+++ C++ Primer/code/第2章/2.30.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	const int v2 = 0;  // Top-level const
 	int v1 = v2;  //  v1 is not a const
-	int i;
 	int *p1 = &v1, &r1 = v1;  // p1 and r1 is not a const
+	int i;
 	const int *p2 = &v2, *const p3 = &i, &r2 = v2;  // p2 is a low-level const. p3 is a both top-level and low-level const. r2 is a low-level const.
 	return 0;
 }
