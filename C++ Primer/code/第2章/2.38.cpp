be45982227af5a618a79394c8ed0762f679cb30c
Andy Cai <1781953879@qq.com>
2021-04-08 11:44:46 +0800

    add C++ Primer code 第2章 2.38.cpp

---
+++ C++ Primer/code/第2章/2.38.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a;
+	decltype(a) a1;  // a1 is a int
+	auto a2 = a;  // a2 is a int
+	int& b = a;
+	decltype(b) b1 = a;  // b1 is a int reference
+	auto b2 = a;  // b2 is a int
+	return 0;
+}
