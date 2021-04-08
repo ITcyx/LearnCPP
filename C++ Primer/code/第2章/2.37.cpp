ba77659df7392e6a5da405046941e074cb81cfa8
Andy Cai <1781953879@qq.com>
2021-04-08 11:41:23 +0800

    add C++ Primer code 第2章 2.37.cpp

---
+++ C++ Primer/code/第2章/2.37.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a = 3, b = 4;
+	decltype(a) c = a;  // c is a int and it is equal to 3
+	decltype(a = b) d = a;  // d is a int reference and it is equal to 3
+	return 0;
+}
