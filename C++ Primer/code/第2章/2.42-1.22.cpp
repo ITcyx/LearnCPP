e97c9b722f4d3dfce70472e362f9ddf2f2396e2c
Andy Cai <1781953879@qq.com>
2021-04-08 22:18:55 +0800

    add C++ Primer code 第2章 2.42-1.22.cpp

---
+++ C++ Primer/code/第2章/2.42-1.22.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "Sales_data.h"
+
+int main()
+{
+	Sales_data item, book;
+	double price;
+	std::cin >> book.bookNo >> book.units_sold >> price;
+	book.revenue = price * book.units_sold;
+	while (std::cin >> item.bookNo >> item.units_sold >> price)
+	{
+		if (item.bookNo == book.bookNo)
+		{
+			item.revenue = price * item.units_sold;
+			book.units_sold += item.units_sold;
+			book.revenue += item.revenue;
+		}
+		else
+		{
+			std::cerr << "Data must refer to the same ISBN" << std::endl;
+			return -1;
+		}
+	}
+	std::cout << book.bookNo << " " << book.units_sold << " " << book.revenue << " ";
+	if (book.units_sold != 0)
+		std::cout << book.revenue / book.units_sold << std::endl;
+	else
+		std::cout << "(no sales)" << std::endl;
+	return 0;
+}
