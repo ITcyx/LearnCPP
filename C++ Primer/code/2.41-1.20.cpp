bdeaad1e0b0d7c5e5da326f479a0895da0efe20e
Andy Cai <1781953879@qq.com>
2021-04-08 12:35:51 +0800

    add C++ Primer code 第2章 2.41-1.20.cpp

---
+++ C++ Primer/code/2.41-1.20.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+struct Sales_data
+{
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+int main()
+{
+	Sales_data book;
+	while (std::cin >> book.bookNo)
+		std::cout << book.bookNo << std::endl;
+	return 0;
+}
