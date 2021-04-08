bbd65ddbbd95e286736045f474b62ffeb16ef044
Andy Cai <1781953879@qq.com>
2021-04-08 11:22:26 +0800

    add C++ Primer code 第2章 2.35.cpp

---
+++ C++ Primer/code/第2章/2.35.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main()
+{
+	const int i = 42;
+	auto j = i;  // j is a int
+	const auto &k = i;  // k is a const int reference
+	auto *p = &i;  // p is a pointer whitch is point to const int
+	const auto j2 = i, &k2 = i;  // j2 is a const int, k2 is a const int reference
+	return 0;
+}
