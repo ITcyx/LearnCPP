1befd0d8ff223121a77eaf75533aa8d4f7d7ce79
Andy Cai <1781953879@qq.com>
2021-04-08 11:16:24 +0800

    add C++ Primer code 第2章 2.34.cpp

---
+++ C++ Primer/code/第2章/2.34.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+	int i = 0, &r = i;
+	auto  a = r;
+	const int ci = i, &cr = ci;
+	auto b = ci;
+	auto c = cr;
+	auto d = &i;
+	auto e = &ci;
+	auto &g = ci;
+	std::cout << r << "   ";
+	a = 42;
+	std::cout << r << std::endl;
+	std::cout << ci << "   ";
+	b = 42;
+	std::cout << ci << std::endl;
+	std::cout << cr << "   ";
+	c = 42;
+	std::cout << cr << std::endl;
+	// d = 42;  // Error
+	// e = 42;  // Error
+	// g = 42;  // Error
+	return 0;
+}
