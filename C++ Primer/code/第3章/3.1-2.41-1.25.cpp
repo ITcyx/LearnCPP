856b9d9387c77860100cc6ee204ffc94cf306668
Andy Cai <1781953879@qq.com>
2021-04-15 13:40:44 +0800

    add C++ Primer code 第3章 3.1-2.41-1.25.cpp

---
+++ C++ Primer/code/第3章/3.1-2.41-1.25.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+
+struct Sales_data
+{
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+int main()
+{
+	Sales_data total;
+	double price;
+	if (cin >> total.bookNo >> total.units_sold >> price)
+	{
+		total.revenue = price * total.units_sold;
+		Sales_data trans;
+		while (cin >> trans.bookNo >> trans.units_sold >> price)
+		{
+			trans.revenue = price * trans.units_sold;
+			if (total.bookNo == trans.bookNo)
+			{
+				total.units_sold += trans.units_sold;
+				total.revenue += trans.revenue;
+			}
+			else
+			{
+				cout << total.bookNo << " " << total.units_sold << " " << total.revenue << " ";
+				if (total.units_sold != 0)
+					cout << total.revenue / total.units_sold << endl;
+				else
+					cout << "(no sales)" << endl;
+				total = trans;
+			}
+		}
+		cout << total.bookNo << " " << total.units_sold << " " << total.revenue << " ";
+		if (total.units_sold != 0)
+			cout << total.revenue / total.units_sold << endl;
+		else
+			cout << "(no sales)" << endl;
+	}
+	else
+	{
+		cerr << "NO data?!" << endl;
+		return -1;
+	}
+	return 0;
+}
