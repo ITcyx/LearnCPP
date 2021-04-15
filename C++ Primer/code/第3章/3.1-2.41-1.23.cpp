28450e5dbe15801cab2eab02ffff3486b648ae09
Andy Cai <1781953879@qq.com>
2021-04-15 13:36:20 +0800

    add C++ Primer code 第3章 3.1-2.41-1.23.cpp

---
+++ C++ Primer/code/第3章/3.1-2.41-1.23.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+struct Sales_data
+{
+	string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+};
+
+int main()
+{
+	Sales_data total, trans;
+	double price;
+	int i = 0;
+	if (cin >> total.bookNo >> total.units_sold >> price)
+	{
+		++i;
+		total.revenue = price * total.units_sold;
+		while (cin >> trans.bookNo >> trans.units_sold >> price)
+		{
+			trans.revenue = price * trans.units_sold;
+			if (total.bookNo == trans.bookNo)
+				++i;
+			else
+			{
+				cout << total.bookNo << " " << i << endl;
+				i = 1;
+				total = trans;
+			}
+		}
+		cout << total.bookNo << " " << i << endl;
+	}
+	return 0;
+}
