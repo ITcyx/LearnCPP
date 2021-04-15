5403d074a0fe9b849e8b054305f59d7d5ab7f86a
Andy Cai <1781953879@qq.com>
2021-04-15 13:23:05 +0800

    add C++ Primer code 第3章 3.1-1.11.cpp

---
+++ C++ Primer/code/第3章/3.1-1.11.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	int num1, num2, val;
+	cout << "Enter two numbers:" << endl;
+	cin >> num1 >> num2;
+	val = num1;
+	while (val <= num2)
+	{
+		cout << val << endl;
+		++val;
+	}
+	return 0;
+}
