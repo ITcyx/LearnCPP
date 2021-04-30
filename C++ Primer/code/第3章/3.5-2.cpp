aff8f0342399155e7987bc6e90c6a91f24733f6d
Andy Cai <1781953879@qq.com>
2021-04-30 22:12:52 +0800

    add C++ Primer code 第3章 3.5-2.cpp

---
+++ C++ Primer/code/第3章/3.5-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	string t, str;
+	cin >> str;
+	while (cin >> t)
+		str += " " + t;
+	cout << str << endl;
+	return 0;
+}
