626c67dc6ef8b53ea6b7a9d457ddb19ca24f4280
Andy Cai <1781953879@qq.com>
2021-04-30 21:53:38 +0800

    add C++ Primer code 第3章 3.4-1.cpp

---
+++ C++ Primer/code/第3章/3.4-1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	string str1, str2;
+	cin >> str1 >> str2;
+	if (str1 == str2)
+		cout << "The strings are the same." << endl;
+	else
+	{
+		cout << "The bigger string is: ";
+		if (str1 > str2)
+			cout << str1 << endl;
+		else
+			cout << str2 << endl;
+	}
+	return 0;
+}
