1a2c3a8986423e491e3f74740d693611e9e28ed3
Andy Cai <1781953879@qq.com>
2021-04-30 22:05:11 +0800

    add C++ Primer code 第3章 3.4-2.cpp

---
+++ C++ Primer/code/第3章/3.4-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+	string str1, str2;
+	cin >> str1 >> str2;
+	if (str1.size() == str2.size())
+		cout << "The strings are the same length." << endl;
+	else
+	{
+		cout << "The longer string is: ";
+		if (str1.size() > str2.size())
+			cout << str1 << endl;
+		else
+			cout << str2 << endl;
+	}
+	return 0;
+}
