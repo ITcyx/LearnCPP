2ea2129adb23f192cff392d84baa04da15aaff6d
Andy Cai <1781953879@qq.com>
2021-04-30 21:18:02 +0800

    add C++ Primer code 第3章 3.3.cpp

---
+++ C++ Primer/code/第3章/3.3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+using std::string;
+using std::getline;
+using std::cin;
+
+int main()
+{
+	string a;
+	getline(cin, a);  // Read all the char until read a line break char
+	cin >> a;  // Read all the char until read a blank char
+	return 0;
+}
